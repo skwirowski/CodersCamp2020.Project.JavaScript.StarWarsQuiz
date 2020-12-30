9d25c02f10621aad878a349b4479bc9967654df9
domq077 <dominik.smutek.51@onet.pl>
2020-12-30 17:44:38 +0100

    fixed inner text to button

--- src/app/redButton.js
+++ src/app/redButton.js
@@ -1,8 +1,9 @@
 export const redButton = (text) => {
     const app = document.querySelector('#swquiz-app');
     const button = document.createElement('button');
+    let buttonText = document.createTextNode(`${text}`);
     button.setAttribute('id', 'red-button')
     button.setAttribute('class', 'button button--red');
-    button.innerText(`${text}`);
+    button.appendChild(buttonText);
     app.appendChild(button);
 }
