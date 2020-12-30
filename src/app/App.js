cafcc991d2a83125efc3fcceaf4168d21a329fde
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2020-12-30 12:15:57 +0100

    show question

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,7 @@
+import {gameMode} from './gameMode'
+
 export const App = ({options}) => {
+    gameMode('Who is this Character?')
 }
 
 
--- src/app/gameMode.js
+++ src/app/gameMode.js
@@ -1,3 +1,12 @@
-const gameMode = () =>{
-    console.log('')
+export const gameMode = (text) =>{
+    const app = document.querySelector('#swquiz-app');
+    const questionDiv = document.createElement('div');
+    
+    questionDiv.id = 'question';
+    questionDiv.className = 'question';
+
+    let questionText = document.createTextNode(`MODE: ${text}`)
+    questionDiv.appendChild(questionText)
+    app.appendChild(questionDiv)
+
 }
