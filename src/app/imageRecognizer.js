00f75783c1d7373d076f4950322a968d126b6c08
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2021-01-02 20:35:49 +0100

    delete comments

--- src/app/imageRecognizer.js
+++ src/app/imageRecognizer.js
@@ -2,7 +2,6 @@ export const imageRecognizer = (img) => {
     const app = document.querySelector('#swquiz-app')
     const imageDiv = document.createElement('div')
     //btoa('string') - encode a string
-    console.log(btoa('static/assets/img/modes/people/36.jpg'))
     //function decodes a string of data which has been encoded using Base64 encoding.
     let image = atob(img) 
     imageDiv.classList.add('swquiz-app__image')
