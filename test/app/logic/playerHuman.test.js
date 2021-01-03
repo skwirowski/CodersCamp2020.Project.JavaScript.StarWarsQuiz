7fe5cb0224659a5df94392e4a441fff4b9ea707a
domq077 <dominik.smutek.51@onet.pl>
2021-01-03 10:19:22 +0100

    fixed bugs after review

--- test/app/logic/playerHuman.test.js
+++ test/app/logic/playerHuman.test.js
@@ -1,17 +1,17 @@
 import { PlayerHuman } from '../../../src/app/logic/playerHuman';
 
-describe(`playerHuman logic checking`, () => {
+describe("playerHuman logic checking", () => {
     const player = new PlayerHuman();
     let question;
     let answer;
 
-    it('Player got question, then askedQuestion should be call with question as parameter', () => {
+    it("Player got a question, then askedQuestion should be called with question as parameter", () => {
         const askedQuestion = jest.fn();
         player.askQuestion(question, askedQuestion);
         expect(askedQuestion).toBeCalledWith(question);
     });
 
-    it(`Player don't get a question, throw err - Player should get question`, () => {
+    it("Player didn't got a question, throw err - Player should get question", () => {
         try {
             expect(player.askQuestion()).toBeCalledWith(question);
         } catch (error) {
@@ -19,13 +19,13 @@ describe(`playerHuman logic checking`, () => {
         }
     });
 
-    it('Player should answered on question, then answerdOnQuestion should be call with answer as parameter', () => {
+    it("Player should answered on question, then answerdOnQuestion should be called with answer as parameter", () => {
         const answerdOnQuestion = jest.fn();
         player.answerOnQuestion(answer, answerdOnQuestion);
         expect(answerdOnQuestion).toBeCalledWith(answer);
     });
 
-    it(`Player don't answered on question, throw err - Player should answer on asked question`, () => {
+    it("Player didn't answered on question, throw err - Player should answer on asked question", () => {
         try {
             expect(player.answerOnQuestion()).toBeCalledWith(answer);
         } catch (error) {
