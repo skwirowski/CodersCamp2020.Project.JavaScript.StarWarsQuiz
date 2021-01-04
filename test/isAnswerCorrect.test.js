234bb3b91a0372dc69a7886e04dff3f6a8b415b4
Jakub Góra <jgora.szczecin@gmail.com>
2021-01-04 20:51:50 +0100

    fixed unit test error message

--- test/isAnswerCorrect.test.js
+++ test/isAnswerCorrect.test.js
@@ -3,12 +3,12 @@ import '@testing-library/jest-dom/extend-expect';
 
 test("Throw error when guessed answer isn't string.",()=>{
     const foo = 2;
-    expect(function(){isAnswerCorrect(foo,"answer");}).toThrow(new Error("Guessed answer isn't string"));
+    expect(function(){isAnswerCorrect(foo,"answer");}).toThrow(new Error("TypeError"));
 })
 
 test("Throw error when correct answer isn't string.",()=>{
     const foo = 2;
-    expect(function(){isAnswerCorrect("guess",foo);}).toThrow(new Error("Correct answer isn't string"));
+    expect(function(){isAnswerCorrect("guess",foo);}).toThrow(new Error("TypeError"));
 })
 
 test("Function return true if answers match.",()=>{
