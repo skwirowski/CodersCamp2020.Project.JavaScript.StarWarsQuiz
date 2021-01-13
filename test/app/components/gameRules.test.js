3f91e53840459c477a9940e4c0f2d20e51dca069
mfrydrychowicz <mamgofry@gmail.com>
2021-01-13 22:34:20 +0100

    [#35] Test added

---
+++ test/app/components/gameRules.test.js
@@ -0,0 +1,20 @@
+import { gameRules } from '../../../src/app/components/gameRules';
+import { screen } from '@testing-library/dom';
+import { PEOPLE_MODE_RULE_STRING } from '../../../src/app/ruleConstantStrings';
+
+test('screen has paragraph with rule text', () => {
+  document.body.innerHTML = `<div id="rules-box" class="rules-box"></div>`;
+  gameRules(PEOPLE_MODE_RULE_STRING);
+
+  let paragraph = document.querySelectorAll('p');
+  expect(paragraph).toBeTruthy();
+  expect();
+});
+
+test('there is an img tag for icon', () => {
+  document.body.innerHTML = `<div id="rules-box" class="rules-box"></div>`;
+  gameRules(PEOPLE_MODE_RULE_STRING);
+
+  expect(screen.getByTestId('rules-icon')).toBeTruthy();
+});
+
