e20d10d45a565a46f7ea00b2bdce61489cfb815f
domq077 <dominik.smutek.51@onet.pl>
2021-01-14 22:02:02 +0100

    component shows transfeered content

--- src/app/components/modalWindow.js
+++ src/app/components/modalWindow.js
@@ -1,3 +1,17 @@
-export const modalWindow = (component) => {
+export const modalWindow = (component, closeWindow) => {
+    if(!(component instanceof HTMLElement)) {
+        throw new Error('First argument should be a HTML element');
+    }
 
+    if(typeof closeWindow !== 'function') {
+        throw new Error('Second argument should be a function');
+    }
+
+    const container = document.createElement('div');
+    container.setAttribute('id', 'modalWindow');
+    container.setAttribute('class', 'modalWindow-container');
+    container.appendChild(component);
+
+
+    return container;
 }
