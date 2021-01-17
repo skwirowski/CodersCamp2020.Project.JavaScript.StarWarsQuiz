bc4e477fd29bd6a99c9fdca2af9283dede4e9f12
Jakub Góra <jgora.szczecin@gmail.com>
2021-01-17 23:43:59 +0100

    [#84] Responsive modal

--- src/app/App.js
+++ src/app/App.js
@@ -2,65 +2,62 @@ import { doc } from 'prettier';
 import { startTime } from './logic/timer';
 import { generateQuestion } from './logic/generatingQuestions/generateQuestion';
 import { modalGameOver } from './components/modalGameOver';
-import { mainWindow } from './components/mainWindow';
+import { computerAnswers, mainWindow, playerAnswers } from './components/mainWindow';
 
-const testDataHuman = {
-  q1: {
+const testDataHuman = [
+  {
     answer: 'Test',
     correct: 'Test',
     isCorrect: true,
     img: '../../static/assets/img/modes/people/1.jpg',
   },
-  q2: {
+  {
     answer: 'Test',
     correct: 'Test',
     isCorrect: true,
     img: '../../static/assets/img/modes/people/10.jpg',
   },
-  q3: {
+  {
     answer: 'Test',
     correct: 'Test',
     isCorrect: true,
     img: '../../static/assets/img/modes/people/10.jpg',
   },
-  q4: {
+  {
     answer: 'Test',
     correct: 'Test',
     isCorrect: true,
     img: '../../static/assets/img/modes/people/10.jpg',
   },
-  correct: 1,
-  total: 2,
-};
+  
+];
 
-const testDataComputer = {
-  q1: {
+const testDataComputer = [
+  {
     answer: 'Test2',
     correct: 'Test',
     isCorrect: false,
     img: '../../static/assets/img/modes/people/1.jpg',
   },
-  q2: {
+  {
     answer: 'Test2',
     correct: 'Test',
     isCorrect: false,
     img: '../../static/assets/img/modes/people/10.jpg',
   },
-  q3: {
+  {
     answer: 'Test',
     correct: 'Test',
     isCorrect: true,
     img: '../../static/assets/img/modes/people/10.jpg',
   },
-  q4: {
+  {
     answer: 'Test',
     correct: 'Test',
     isCorrect: true,
     img: '../../static/assets/img/modes/people/10.jpg',
   },
-  correct: 0,
-  total: 2,
-};
+];
 
 
 const modalSubmitCallback = (e) => {
