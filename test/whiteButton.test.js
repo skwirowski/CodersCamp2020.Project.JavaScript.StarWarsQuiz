df0c5238ae2d133af3300134037120ebb9adb1d4
mfrydrychowicz <mamgofry@gmail.com>
2021-01-17 21:04:22 +0100

    Filename fix + img test

--- test/whiteButton.test.js
+++ test/whiteButton.test.js
@@ -5,7 +5,7 @@ import '@testing-library/jest-dom/extend-expect';
 test('button has class button button--white ', () => {
   document.body.innerHTML = `<div id="swquiz-app"></div>`;
   const testText = 'Hall of fame';
-  const testIcon = 'static/assets/icons/contacts_24px.png'
+  const testIcon = 'static/assets/icons/contacts_24px.svg'
   whiteButton(testIcon, testText);
   expect(screen.getByTestId('white-button')).toHaveClass(
     'button button--white ',
@@ -15,18 +15,24 @@ test('button has class button button--white ', () => {
 test('button has text', () => {
   document.body.innerHTML = `<div id="swquiz-app"></div>`;
   const testText = 'Hall of fame';
-  const testIcon = 'static/assets/icons/contacts_24px.png'
+  const testIcon = 'static/assets/icons/contacts_24px.svg'
   whiteButton(testIcon, testText);
   expect(screen.getByTestId('white-button')).not.toBeEmptyDOMElement();
-  
 });
 
-  test('button has text - Hall of fame', () => {
-    document.body.innerHTML = `<div id="swquiz-app"></div>`;
-    const testText = 'Hall of fame';
-    const testIcon = 'static/assets/icons/contacts_24px.png'
-    whiteButton(testIcon,testText);
-    expect(screen.getByTestId('white-button')).toHaveTextContent('Hall of fame');
-  });
+test('button has text - Hall of fame', () => {
+  document.body.innerHTML = `<div id="swquiz-app"></div>`;
+  const testText = 'Hall of fame';
+  const testIcon = 'static/assets/icons/contacts_24px.svg'
+  whiteButton(testIcon,testText);
+  expect(screen.getByTestId('white-button')).toHaveTextContent('Hall of fame');
+});
 
-
+test('button has icon', () => {
+  document.body.innerHTML = `<div id="swquiz-app"></div>`;
+  const testText = 'Hall of fame';
+  const testIcon = 'static/assets/icons/contacts_24px.svg'
+  whiteButton(testIcon, testText);
+  let button = screen.getByTestId('white-button');
+  expect(button.querySelector('img')).toBeTruthy();
+});
