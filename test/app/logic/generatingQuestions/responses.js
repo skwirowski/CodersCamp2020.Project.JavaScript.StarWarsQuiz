9f9acd20f467fbff628562f6ec902a774f789fe5
mfrydrychowicz <mamgofry@gmail.com>
2021-01-18 18:18:50 +0100

    [#67] Create project readme (#81)
    * Https

--- test/app/logic/generatingQuestions/responses.js
+++ test/app/logic/generatingQuestions/responses.js
@@ -7,25 +7,25 @@ export const id1Response = {
     "eye_color": "blue", 
     "birth_year": "19BBY", 
     "gender": "male", 
-    "homeworld": "http://swapi.dev/api/planets/1/", 
+    "homeworld": "https://swapi.dev/api/planets/1/", 
     "films": [
-        "http://swapi.dev/api/films/1/", 
-        "http://swapi.dev/api/films/2/", 
-        "http://swapi.dev/api/films/3/", 
-        "http://swapi.dev/api/films/6/"
+        "https://swapi.dev/api/films/1/", 
+        "https://swapi.dev/api/films/2/", 
+        "https://swapi.dev/api/films/3/", 
+        "https://swapi.dev/api/films/6/"
     ], 
     "species": [], 
     "vehicles": [
-        "http://swapi.dev/api/vehicles/14/", 
-        "http://swapi.dev/api/vehicles/30/"
+        "https://swapi.dev/api/vehicles/14/", 
+        "https://swapi.dev/api/vehicles/30/"
     ], 
     "starships": [
-        "http://swapi.dev/api/starships/12/", 
-        "http://swapi.dev/api/starships/22/"
+        "https://swapi.dev/api/starships/12/", 
+        "https://swapi.dev/api/starships/22/"
     ], 
     "created": "2014-12-09T13:50:51.644000Z", 
     "edited": "2014-12-20T21:17:56.891000Z", 
-    "url": "http://swapi.dev/api/people/1/"
+    "url": "https://swapi.dev/api/people/1/"
 }
   
 export const id2Response = {
@@ -37,23 +37,23 @@ export const id2Response = {
     "eye_color": "yellow", 
     "birth_year": "112BBY", 
     "gender": "n/a", 
-    "homeworld": "http://swapi.dev/api/planets/1/", 
+    "homeworld": "https://swapi.dev/api/planets/1/", 
     "films": [
-        "http://swapi.dev/api/films/1/", 
-        "http://swapi.dev/api/films/2/", 
-        "http://swapi.dev/api/films/3/", 
-        "http://swapi.dev/api/films/4/", 
-        "http://swapi.dev/api/films/5/", 
-        "http://swapi.dev/api/films/6/"
+        "https://swapi.dev/api/films/1/", 
+        "https://swapi.dev/api/films/2/", 
+        "https://swapi.dev/api/films/3/", 
+        "https://swapi.dev/api/films/4/", 
+        "https://swapi.dev/api/films/5/", 
+        "https://swapi.dev/api/films/6/"
     ], 
     "species": [
-        "http://swapi.dev/api/species/2/"
+        "https://swapi.dev/api/species/2/"
     ], 
     "vehicles": [], 
     "starships": [], 
     "created": "2014-12-10T15:10:51.357000Z", 
     "edited": "2014-12-20T21:17:50.309000Z", 
-    "url": "http://swapi.dev/api/people/2/"
+    "url": "https://swapi.dev/api/people/2/"
 }
   
 export const id3Response = {
@@ -65,23 +65,23 @@ export const id3Response = {
     "eye_color": "red", 
     "birth_year": "33BBY", 
     "gender": "n/a", 
-    "homeworld": "http://swapi.dev/api/planets/8/", 
+    "homeworld": "https://swapi.dev/api/planets/8/", 
     "films": [
-        "http://swapi.dev/api/films/1/", 
-        "http://swapi.dev/api/films/2/", 
-        "http://swapi.dev/api/films/3/", 
-        "http://swapi.dev/api/films/4/", 
-        "http://swapi.dev/api/films/5/", 
-        "http://swapi.dev/api/films/6/"
+        "https://swapi.dev/api/films/1/", 
+        "https://swapi.dev/api/films/2/", 
+        "https://swapi.dev/api/films/3/", 
+        "https://swapi.dev/api/films/4/", 
+        "https://swapi.dev/api/films/5/", 
+        "https://swapi.dev/api/films/6/"
     ], 
     "species": [
-        "http://swapi.dev/api/species/2/"
+        "https://swapi.dev/api/species/2/"
     ], 
     "vehicles": [], 
     "starships": [], 
     "created": "2014-12-10T15:11:50.376000Z", 
     "edited": "2014-12-20T21:17:50.311000Z", 
-    "url": "http://swapi.dev/api/people/3/"
+    "url": "https://swapi.dev/api/people/3/"
 }
   
 export const id4Response = {
@@ -93,20 +93,20 @@ export const id4Response = {
     "eye_color": "yellow", 
     "birth_year": "41.9BBY", 
     "gender": "male", 
-    "homeworld": "http://swapi.dev/api/planets/1/", 
+    "homeworld": "https://swapi.dev/api/planets/1/", 
     "films": [
-        "http://swapi.dev/api/films/1/", 
-        "http://swapi.dev/api/films/2/", 
-        "http://swapi.dev/api/films/3/", 
-        "http://swapi.dev/api/films/6/"
+        "https://swapi.dev/api/films/1/", 
+        "https://swapi.dev/api/films/2/", 
+        "https://swapi.dev/api/films/3/", 
+        "https://swapi.dev/api/films/6/"
     ], 
     "species": [], 
     "vehicles": [], 
     "starships": [
-        "http://swapi.dev/api/starships/13/"
+        "https://swapi.dev/api/starships/13/"
     ], 
     "created": "2014-12-10T15:18:20.704000Z", 
     "edited": "2014-12-20T21:17:50.313000Z", 
-    "url": "http://swapi.dev/api/people/4/"
+    "url": "https://swapi.dev/api/people/4/"
 }
   
