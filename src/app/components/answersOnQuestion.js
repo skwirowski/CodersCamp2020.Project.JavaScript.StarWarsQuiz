542d9980a44b089afe381a89496898a2c04df97a
Ryczko <konradryczko1@gmail.com>
2021-01-18 18:20:50 +0100

    answer button bug fixed

--- src/app/components/answersOnQuestion.js
+++ src/app/components/answersOnQuestion.js
@@ -9,7 +9,7 @@ let nextQuestion = {};
 let computerPlayer = new playerComputer();
 // let player = new PlayerHuman();
 
-export const answersOnQuestion = (answerObject, target) => {    
+export const answersOnQuestion = (answerObject, target) => {
     const app = document.querySelector('#swquiz-game-body');
     const container = document.createElement('div');
     container.setAttribute('id', 'answers-container');
@@ -18,49 +18,50 @@ export const answersOnQuestion = (answerObject, target) => {
     let chosenAnswer;
     let ids = ['firstAnswer', 'secondAnswer', 'thirdAnswer', 'fourthAnswer'];
     let buttons = ids.map((idString, index) => {
-      let button = document.createElement('input');
-      button.setAttribute('type', 'button');
-      button.setAttribute('id', idString);
-      button.setAttribute('data-testid', idString);
-      button.setAttribute('class', 'button button--lighter');
-      button.setAttribute('value', `${answerObject.answers[index]}`);
-      return button;
+        let button = document.createElement('input');
+        button.setAttribute('type', 'button');
+        button.setAttribute('id', idString);
+        button.setAttribute('data-testid', idString);
+        button.setAttribute('class', 'button button--lighter');
+        button.setAttribute('value', `${answerObject.answers[index]}`);
+        return button;
     });
-    
+
     buttons.forEach((element) => {
-      container.appendChild(element);
+        container.appendChild(element);
     });
     app.appendChild(container);
 
 
     const buttonsContainer = document.getElementById('answers-container');
 
-    buttonsContainer.addEventListener('click', (e)=>{
-        if(e.target.id === 'answers-container') return;
-            chosenAnswer = e.target.value;
-            let returnImage = answerObject.image || nextQuestion.image
-            let computerAnswer
-            playerAnswers.push({answer: chosenAnswer, correct: answerObject.rightAnswer, isCorrect: isAnswerCorrect(chosenAnswer, answerObject.rightAnswer), img:`data:image/png;base64,${returnImage}`});
-            // player.addAnswer({answer: chosenAnswer, correct: answerObject.rightAnswer, isCorrect: isAnswerCorrect(chosenAnswer, answerObject.rightAnswer), img:`data:image/png;base64,${returnImage}`})
-            // console.log(player.getAnswer())
-            computerAnswer = computerPlayer.chooseRandom(answerObject.answers)
-            computerAnswers.push({answer: computerAnswer, correct: answerObject.rightAnswer, isCorrect: isAnswerCorrect(computerAnswer, answerObject.rightAnswer), img:`data:image/png;base64,${returnImage}`})
-            if (answerObject.rightAnswer === chosenAnswer) {
-                e.target.classList.add('button--correct');
-            } 
-            else {
-                e.target.classList.add('button--wrong');
-            }
-            window.setTimeout(function(){
-                container.remove();
-                answersOnQuestion(nextQuestion, target)
-                imageRecognizer(nextQuestion.image)
-            }, 1000);
 
-            generateQuestion(target).then(res=> {
-                nextQuestion = res;
-            })
-    }, {
-        once: true
-      })
+    const onButtonClick = (e) => {
+        if (e.target.id === 'answers-container') return;
+        chosenAnswer = e.target.value;
+        let returnImage = answerObject.image || nextQuestion.image
+        let computerAnswer
+        playerAnswers.push({ answer: chosenAnswer, correct: answerObject.rightAnswer, isCorrect: isAnswerCorrect(chosenAnswer, answerObject.rightAnswer), img: `data:image/png;base64,${returnImage}` });
+
+        computerAnswer = computerPlayer.chooseRandom(answerObject.answers)
+        computerAnswers.push({ answer: computerAnswer, correct: answerObject.rightAnswer, isCorrect: isAnswerCorrect(computerAnswer, answerObject.rightAnswer), img: `data:image/png;base64,${returnImage}` })
+        if (answerObject.rightAnswer === chosenAnswer) {
+            e.target.classList.add('button--correct');
+        }
+        else {
+            e.target.classList.add('button--wrong');
+        }
+        window.setTimeout(function () {
+            container.remove();
+            answersOnQuestion(nextQuestion, target)
+            imageRecognizer(nextQuestion.image)
+        }, 1000);
+
+        generateQuestion(target).then(res => {
+            nextQuestion = res;
+        })
+        buttonsContainer.removeEventListener('click', onButtonClick);
+    }
+
+    buttonsContainer.addEventListener('click', onButtonClick)
 }
