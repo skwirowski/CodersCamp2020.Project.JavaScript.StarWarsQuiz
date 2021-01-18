de0876755020de2dd326914681480d11db3c3fbb
Jakub Góra <jgora.szczecin@gmail.com>
2021-01-18 20:28:38 +0100

    removed redundant code

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,6 @@
 import { doc } from 'prettier';
 import { mainWindow } from './components/mainWindow';
-import {modalGameOver} from './components/modalGameOver';
-import { playerAnswers, computerAnswers } from './components/mainWindow'
-
 
 export const App = ({ options }) => {
   mainWindow(options.quizMaxTime);
-//   modalGameOver(
-//     document.querySelector('#swquiz-app'),
-//     playerAnswers,
-//     computerAnswers,
-//     function(){},
-// );
 };
